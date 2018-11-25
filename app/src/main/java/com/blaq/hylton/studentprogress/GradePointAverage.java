c99bcf45aee9d40a2e244a1bdf26dff6ef4e257a
blaq-63 <hilton_carboo@student.uml.edu>
2018-11-25 12:22:05 -0500

    Created Unique IDs for view that are created dynamically.

--- app/src/main/java/com/blaq/hylton/studentprogress/GradePointAverage.java
+++ app/src/main/java/com/blaq/hylton/studentprogress/GradePointAverage.java
@@ -1,9 +1,8 @@
 package com.blaq.hylton.studentprogress;
 
-import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,51 +14,44 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class GradePointAverage extends Fragment
 {
     private Spinner mSpinner1;
-    private Spinner mSpinner2;
-    private Spinner mSpinner3;
-    private Spinner mSpinner4;
+
+    private EditText mEditText1;
 
     private Button mButton;
+    private FloatingActionButton fab;
     private LinearLayout masterLinearLayout;
+
     private int counter;
+    private int creditIndex;
+    private int creditsID;
+
+    private ArrayList<Integer> idList;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_grade_point_average, container, false);
 
         mSpinner1 = view.findViewById(R.id.grades_spinner_1);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R
-                .array.grade_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R.array.grade_array, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner1.setAdapter(adapter1);
 
-        mSpinner2 = view.findViewById(R.id.grades_spinner_2);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(), R
-                .array.grade_array, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner2.setAdapter(adapter2);
-
-        mSpinner3 = view.findViewById(R.id.grades_spinner_3);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(getContext(), R
-                .array.grade_array, android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner3.setAdapter(adapter3);
-
-        mSpinner4 = view.findViewById(R.id.grades_spinner_4);
-        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(getContext(), R
-                .array.grade_array, android.R.layout.simple_spinner_item);
-        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner4.setAdapter(adapter4);
-
         mButton = view.findViewById(R.id.button);
+        fab = view.findViewById(R.id.floating_action_button);
         masterLinearLayout = view.findViewById(R.id.linear_layout_master);
+
         counter = masterLinearLayout.getChildCount();
+        creditsID = 0;
+        creditIndex = 0;
+
+        idList = new ArrayList<>();
+        idList.add(creditIndex, R.id.credit_1);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,31 +65,37 @@ public class GradePointAverage extends Fragment
                 else
                 {
                     counter = counter + 1;
-                    createView(masterLinearLayout, counter);
-                    Log.d("Main Activity", "onClick: " + masterLinearLayout.getChildCount());
+                    createView(masterLinearLayout, counter, idList);
                 }
+            }
+        });
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                for (int i = 0; i < masterLinearLayout.getChildCount(); i++)
+                {
+                    Log.d("FAB", "onClick: id = " + idList.get(i) + " for pos " + i);
+                }
             }
         });
 
         return view;
     }
 
-    private void createView(LinearLayout bossLayout, int counter)
+    private void createView(LinearLayout bossLayout, int counter, ArrayList<Integer> idList)
     {
+        creditIndex = creditIndex + 1;
+
         //Create linear layout first
         LinearLayout linearLayout = new LinearLayout(getContext());
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setPadding(4, 4, 4, 4);
 
         //Create Course EditText
-        int id = 0;
         EditText courseEditText = new EditText(getContext());
         courseEditText.setWidth(260);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            id = View.generateViewId();
-            courseEditText.setId(id);
-            Log.d("Main Activity", "createView: " + id);
-        }
         courseEditText.setHint("Course " + counter);
 
         //Create Spinner and populate it
@@ -108,17 +106,37 @@ public class GradePointAverage extends Fragment
         spinner.setPadding(4, 0, 4, 0);
 
         //Create Credit EditText
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+        {
+            creditsID = View.generateViewId();
+        }
+
         String hint = "Credit " + counter;
         EditText creditEditText = new EditText(getContext());
         creditEditText.setHint(hint);
         creditEditText.setWidth(420);
-        //creditEditText.setId(Integer.valueOf(hint));
+        creditEditText.setId(creditsID);
+        Log.d("creditEditText", "createView: " + creditsID);
 
         // Adding to the Child Layout
         linearLayout.addView(courseEditText);
         linearLayout.addView(spinner);
         linearLayout.addView(creditEditText);
 
+        //Sets new ID to newly created EditText field
+        idList.add(creditIndex, creditsID);
+
         bossLayout.addView(linearLayout);
+    }
+
+    public double calculateGPA()
+    {
+
+        return 0;
+    }
+
+    public void getAllID()
+    {
+
     }
 }
